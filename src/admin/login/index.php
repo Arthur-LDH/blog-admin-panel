c788bcec1be6e04637fd9a4192d5e3e3f02cd33b
Arthur-LDH <arthurldh@gmail.com>
2022-06-10 01:04:47 +0200

    Entrée

--- src/admin/login/index.php
+++ src/admin/login/index.php
@@ -6,6 +6,28 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     </head>
+    <?php
+        require('../../config/config.php');
+
+        session_start();
+
+        if (isset($_POST['username'])){
+            $username = stripslashes($_REQUEST['username']);
+            $username = mysqli_real_escape_string($conn, $username);
+            $password = stripslashes($_REQUEST['password']);
+            $password = mysqli_real_escape_string($conn, $password);
+            $query = "SELECT * FROM `users` WHERE username='$username' and password='".hash('sha256', $password)."'";
+            $result = mysqli_query($conn,$query) or die(mysql_error());
+            $rows = mysqli_num_rows($result);
+
+            if($rows==1){
+                $_SESSION['username'] = $username;
+                header("Location: index.php");
+            }else{
+                $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+            }
+        }
+    ?>
     <body>
         <main class="container">
             <div class="row justify-content-center align-items-center" style="min-height:100vh ;">
@@ -23,6 +45,7 @@
                     <label class="form-check-label" for="exampleCheck1">Remember me</label>
                     </div>
                     <button type="submit" class="btn btn-primary">Login</button>
+                    <a href="../../index.html">Home</a>
                 </form>
             </div>
         </main>
